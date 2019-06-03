c196a6b0d677615b37bbee94031f52545930124a
Adrian Camacho <17981466@itroque.edu.mx>
2019-06-03 03:35:43 -0500

    Salas

--- Web/Servicios/InsertarSala.php
+++ Web/Servicios/InsertarSala.php
@@ -6,27 +6,25 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-include_once('../Utilerias/BaseDatos.php');
+include_once('../../utilerias/BaseDatos.php');
 //print_r(insertUser(array('correo'=>'nuevo@gamail.com','nombre'=>'nuevo','pass'=>'123')));
 $data=json_decode(file_get_contents('php://input'));
-if( !empty($data->correo)&&
-    !empty($data->contrasena)){
-        $params=array('correo'=>$data->correo, 'contrasena'=>$data->contrasena);
-        
-        $res=checkUsers($params);
-        
+if( !empty($data->nom_sala) &&
+    !empty($data->capacidad_sala){
+        $params=array('nom_sala'=>$data->nom_sala,'capacidad_sala'=>$data->capacidad_sala);
+        $res=insertRoom($params);
         if($res!=null){
             http_response_code(201);
             echo json_encode(array(
                 'status'=>true,
-                'usr'=>$res,
-                'msg'=>'Usuario Exitente'
+                'room'=>$res,
+                'msg'=>'Sala Insertada'
             ));
         }else{
-            http_response_code(503); 
+            http_response_code(503); //Servicio no disponible
             echo json_encode(array(
                 'status'=>false,
-                'msg'=>'Usuario no Existente'
+                'msg'=>'No se puede Agregar la Sala'
             ));
         }
     }
@@ -34,7 +32,7 @@ else{
     http_response_code(404);
     echo json_encode(array(
         "status"=>false,
-        "msg"=>'Verifique los Parametros'
+        "msg"=>'Verifique los Paramatros'
     ));
 }
 ?>
--- Web/Utilerias/BaseDatos.php
+++ Web/Utilerias/BaseDatos.php
@@ -118,8 +118,10 @@ function insertUser($data){
     return EjecutaConsecutivo($query,"rfc");
 }
 
-function checkUsers($correo,$contrasena){
-    $query="SELECT correo, contrasena FROM adyma WHERE correo='$correo' AND contrasena='$contrasena'";
+function checkUsers($data){
+    $correo = $data['correo'];
+    $contrasena = $data['contrasena'] 
+    $query="SELECT correo, contrasena FROM usuario WHERE correo='$correo' AND contrasena='$contrasena'";
     $result=Consulta($query);
     if(count($result)>0){
         return true;
@@ -139,6 +141,22 @@ function insertRequest($data){
     $query="INSERT INTO solcitud (rfc, id_dependencia, asistecia, id_sala, material, fecha_evento, duracion_evento)
             VALUES ('$rfc', '$id_dep', '$asi', '$id_sal', '$mat', '$fec_eve', '$dur_eve') RETURNING id_solicitud";
     return EjecutaConsecutivo($query,"id_solicitud");
+}
+
+function insertRoom($data){
+    $nom_sala = $data['nom_sala'];
+    $cap_sala = $data['cap_sala'];
+    $query="INSERT INTO sala (nom_sala, cap_sala) VALUES ('$nom_sala', '$cap_sala') RETURNING id_sala";
+    return EjecutaConsecutivo($query,"id_sala");
+}
+
+function getRoom($data){
+        $res=Consulta("SELECT * FROM sala");
+        $response=array();
+        foreach ($res as $key => $value) {
+            $response[$key]=$value;
+        }
+        return $response;
 }
 
 ?>
