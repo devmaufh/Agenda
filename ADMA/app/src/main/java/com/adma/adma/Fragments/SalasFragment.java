f6e3bf4b7ea0705f0c8938a4ebd2922f9c01f57f
Mauricio Flores Hernández <mau1361317@gmail.com>
2019-06-03 03:48:22 -0500

    android changes

--- ADMA/app/src/main/java/com/adma/adma/Fragments/SalasFragment.java
+++ ADMA/app/src/main/java/com/adma/adma/Fragments/SalasFragment.java
@@ -4,6 +4,7 @@ package com.adma.adma.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,12 +16,12 @@ import com.adma.adma.R;
  * A simple {@link Fragment} subclass.
  */
 public class SalasFragment extends Fragment {
+    private RecyclerView rvSalas;
 
 
     public SalasFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
